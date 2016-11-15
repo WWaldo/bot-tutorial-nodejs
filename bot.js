ae6698e3686e2ff7fc795304e75072bfb2abc4b2
WWaldo <hoskinsw@email.sc.edu>
2016-11-15 18:32:11 -0500

    Trying regex again

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = .*;
+      botRegex = /^\.*$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
@@ -21,7 +21,7 @@ function respond() {
 function postMessage(txt) {
   var botResponse, options, body, botReq;
 
-  botResponse = txt;
+  botResponse = txt.slice(1);
   console.log(txt);
 
   options = {
